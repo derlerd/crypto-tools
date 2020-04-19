22d44313b57dc3161c7fd592db15ac2366018df3
David Derler <david@derler.info>
2020-04-19 21:18:31 +0200

    Rework abstractions: separate sigma protocol so that one can do compositions of sigma protocols later

--- src/zkproofs/mod.rs
+++ src/zkproofs/mod.rs
@@ -20,11 +20,19 @@ pub struct DlogEqWitness<'a> {
     x: &'a Scalar,
 }
 
+pub struct DlogEqCommitment {
+	c1: RistrettoPoint,
+	c2: RistrettoPoint
+}
+
+pub struct DlogEqProverState(Scalar);
+pub struct DlogEqChallenge(Scalar);
+pub struct DlogEqResponse(Scalar);
+
 pub struct DlogEqProof {
-    c1: RistrettoPoint,
-    c2: RistrettoPoint,
-    challenge: Scalar,
-    response: Scalar,
+	commitment: DlogEqCommitment,
+    challenge: DlogEqChallenge,
+    response: DlogEqResponse,
 }
 
 impl<'a> DlogEqStatement<'a> {
@@ -54,6 +62,53 @@ impl<'a> DlogEqWitness<'a> {
     }
 }
 
+pub trait SigmaProtocol<RNG, S, W, COM, ST, CH, RSP> {
+  	fn commit(statement : &S, witness : &W, rng : &mut RNG) -> Option<(COM, ST)>;
+  	fn challenge(statement : &S, commitment : &COM, rng : &mut RNG) -> CH;
+  	fn response(statement : &S, witness : &W, challenge : &CH, state : &ST) -> RSP;
+  	fn check(statement : &S, commitment : &COM, challenge : &CH, response : &RSP) -> bool;
+}
+
+impl<RNG : RngCore + CryptoRng> SigmaProtocol<RNG, DlogEqStatement<'_>, DlogEqWitness<'_>, DlogEqCommitment, DlogEqProverState, DlogEqChallenge, DlogEqResponse> for DlogEq<RNG>
+{
+	fn commit(statement : &DlogEqStatement, witness : &DlogEqWitness, rng : &mut RNG) -> Option<(DlogEqCommitment, DlogEqProverState)> {
+		if statement.verify(witness) != true {
+    		return None;
+    	}
+         
+        let r = Scalar::random(rng);
+        let c1 = &r * statement.g_1;
+        let c2 = &r * statement.g_2;
+    	
+    	let state = DlogEqProverState(r);
+    	let commitments = DlogEqCommitment { c1 : c1, c2 : c2 };
+
+    	Some((commitments, state))
+	}
+
+	fn challenge(statement : &DlogEqStatement, commitment : &DlogEqCommitment, rng : &mut RNG) -> DlogEqChallenge {
+        DlogEqChallenge(Scalar::one()) // TODO replace this with hash of challenge
+	}
+
+	fn response(_statement : &DlogEqStatement, witness : &DlogEqWitness, challenge : &DlogEqChallenge, state : &DlogEqProverState) -> DlogEqResponse {
+		DlogEqResponse(&state.0 + witness.x * challenge.0)
+	}
+
+	fn check(statement : &DlogEqStatement, commitment : &DlogEqCommitment, challenge : &DlogEqChallenge, response : &DlogEqResponse) -> bool {
+		let g_1s = statement.g_1 * response.0;
+        let g_2s = statement.g_2 * response.0;
+
+        let g_1v = commitment.c1 + statement.h_1 * challenge.0;
+        let g_2v = commitment.c2 + statement.h_2 * challenge.0; 
+
+        if &g_1s == &g_1v && &g_2s == &g_2v { //TODO verify challenge
+        	return true;
+        } 
+        false
+	}
+
+}
+
 pub trait ProofSystem<RNG, S, W, P> {
     fn prove(statement: &S, witness: &W, rng: &mut RNG) -> Option<P>;
     fn verify(statement: &S, proof: &P) -> bool;
@@ -63,32 +118,20 @@ impl<RNG: RngCore + CryptoRng> ProofSystem<RNG, DlogEqStatement<'_>, DlogEqWitne
     for DlogEq<RNG>
 {
     fn prove(statement: &DlogEqStatement, witness: &DlogEqWitness, rng: &mut RNG) -> Option<DlogEqProof> {
-    	if statement.verify(witness) != true {
-    		return None;
-    	}
+    	let (com, st) = match DlogEq::<RNG>::commit(statement, witness, rng) {
+    		Some((com, st)) => (com, st),
+    		None => return None,
+    	};
 
-        let r = Scalar::random(rng);
-        let c1 = &r * statement.g_1;
-        let c2 = &r * statement.g_2;
-
-        let ch = Scalar::random(rng); // TODO replace this with hash of challenge
-
-        let s = &r + witness.x * ch;
+        let ch = DlogEq::<RNG>::challenge(statement, &com, rng); // replace with RO challenge generation
+       
+        let rsp = DlogEq::<RNG>::response(statement, witness, &ch, &st);
  
-        Some(DlogEqProof { c1 : c1, c2 : c2, challenge : ch, response : s })
+        Some(DlogEqProof { commitment : com, challenge : ch, response : rsp })
     }
 
     fn verify(statement: &DlogEqStatement, proof: &DlogEqProof) -> bool {
-        let g_1s = statement.g_1 * proof.response;
-        let g_2s = statement.g_2 * proof.response;
-
-        let g_1v = proof.c1 + statement.h_1 * proof.challenge;
-        let g_2v = proof.c2 + statement.h_2 * proof.challenge; 
-
-        if &g_1s == &g_1v && &g_2s == &g_2v { //TODO verify challenge
-        	return true;
-        } 
-        false
+      DlogEq::<RNG>::check(statement, &proof.commitment, &proof.challenge, &proof.response)
     }
 }
 
