489ee8647b024bfcd21231bb551b9626d6f8bdee
David Derler <david@derler.info>
2020-06-01 13:06:16 +0200

    Proper error handling for proof systems

--- src/zkproofs/mod.rs
+++ src/zkproofs/mod.rs
@@ -7,17 +7,31 @@ use crate::zkproofs::sigma_protocols::dlogeq::DlogEq;
 use crate::zkproofs::sigma_protocols::fiat_shamir::FsConvertibleSigmaProtocol;
 use crate::zkproofs::sigma_protocols::or_composition::OrComposedSigmaProtocol;
 use crate::zkproofs::sigma_protocols::SigmaProtocol;
+use crate::zkproofs::sigma_protocols::Error as SigmaProtocolError;
 
 use crate::hashing::Hashable;
 
 use sha2::Sha512;
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidWitness
+}
+
+impl From<SigmaProtocolError> for Error {
+    fn from(error : SigmaProtocolError) -> Error {
+        match error {
+          SigmaProtocolError::InvalidWitness => Error::InvalidWitness
+        }
+    }
+}
 
 pub trait ProofSystem<RNG> {
     type S;
     type W;
     type P;
 
-    fn prove(statement: &Self::S, witness: &Self::W, rng: &mut RNG) -> Option<Self::P>;
+    fn prove(statement: &Self::S, witness: &Self::W, rng: &mut RNG) -> Result<Self::P, Error>;
     fn verify(statement: &Self::S, proof: &Self::P) -> bool;
 }
 
@@ -32,23 +46,36 @@ where
     type W = SP::W;
     type P = SP::FSP;
 
-    fn prove(statement: &Self::S, witness: &Self::W, rng: &mut RNG) -> Option<Self::P> {
-        let (com, st) = match SP::commit(statement, witness, rng) {
-            Ok((com, st)) => (com, st),
-            Err(_) => return None,
-        };
-
+    fn prove(statement: &Self::S, witness: &Self::W, rng: &mut RNG) -> Result<Self::P, Error> {
+        let (com, st) = SP::commit(statement, witness, rng)?;
         let ch = SP::hash_challenge(statement, &com);
-
         let rsp = SP::response(statement, witness, &ch, st);
 
-        Some(SP::compile_proof(com, rsp))
+        Ok(SP::compile_proof(com, rsp))
     }
 
     fn verify(statement: &Self::S, proof: &Self::P) -> bool {
         let (commitment, response) = SP::unwrap_proof(proof);
         let ch = SP::hash_challenge(statement, &commitment);
         SP::check(statement, &commitment, &ch, &response)
+    }
+}
+
+impl std::error::Error for Error 
+{   
+ fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
+
+impl std::fmt::Display for Error 
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+      match self {
+        Error::InvalidWitness => {
+            write!(f, "The given witness does not attest membership of the statement in the language.")
+        }
+      }
     }
 }
 
