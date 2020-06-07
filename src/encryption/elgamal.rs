8781c965bd76bed740f90512251e974e00a6760f
David Derler <david@derler.info>
2020-06-07 16:33:01 +0200

    Add documentation for encryption submodule

--- src/encryption/elgamal.rs
+++ src/encryption/elgamal.rs
@@ -72,6 +72,8 @@ impl super::PublicKey for ElGamalPublicKey {
     }
 }
 
+/// Implementation of the [ElGamal](https://doi.org/10.1007%2FBFb0054851) 
+/// public key encryption scheme.
 impl super::EncryptionScheme for ElGamal {
     type SK = ElGamalSecretKey;
     type PK = ElGamalPublicKey;
--- src/encryption/mod.rs
+++ src/encryption/mod.rs
@@ -4,38 +4,66 @@ use rand::{CryptoRng, RngCore};
 
 #[derive(Debug)]
 pub enum Error {
+    /// The given key length in bytes is not supported by the implementation.
     UnsupportedKeyLength(u32),
 }
 
+/// Represents an encryption secret key.
 pub trait SecretKey {
+    /// Generate a secret key of the length in bytes given in the `key_len`
+    /// using the given RNG `rng`. Return an [Error](enum.Error.html) in case
+    /// the given key length is not supported.
     fn generate<RNG: RngCore + CryptoRng>(key_len: u32, rng: &mut RNG) -> Result<Self, Error>
     where
         Self: Sized;
 }
 
+/// Represents an encryption public key. We work under the assumption that
+/// a public key is uniquely determined by the secret key.
 pub trait PublicKey {
+    /// The type of the corresponding secret key.
     type SK;
 
+    /// Generate a public key from a given secret key.
     fn from_secret(secret_key: &Self::SK) -> Self
     where
         Self: Sized;
 }
 
+/// Represents a public key encryption scheme. The interface follows 
+/// the interface as it is commonly used in the literature (see, e.g., 
+/// [KL'14](http://www.cs.umd.edu/~jkatz/imc.html)) 
 pub trait EncryptionScheme {
+    /// The secret key space
     type SK;
+
+    /// The public key space
     type PK;
+
+    /// The message space
     type MSG;
+
+    /// The ciphertext space
     type CTXT;
 
+    /// Takes a key length `key_len` and a RNG `rng` and generates an
+    /// encryption key pair. It fails with an `UnsupportedKeylength` 
+    /// error in case the given key length is not supported.
     fn key_gen<RNG: RngCore + CryptoRng>(
         key_len: u32,
         rng: &mut RNG,
     ) -> Result<(Self::SK, Self::PK), Error>;
+
+    /// Takes a public key `public_key`, a message `message` and a
+    /// RNG `rng` and returns a ciphertext.
     fn encrypt<RNG: RngCore + CryptoRng>(
         public_key: &Self::PK,
         message: Self::MSG,
         rng: &mut RNG,
     ) -> Self::CTXT;
+
+    /// Takes a secret key `secret_key` and a ciphertext `ciphertext`,
+    /// and returns the decrypted message.
     fn decrypt(secret_key: &Self::SK, ciphertext: Self::CTXT) -> Self::MSG;
 }
 
