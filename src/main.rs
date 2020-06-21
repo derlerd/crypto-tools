e6d43bd4241c865dfdea36df02cbfa023641935b
David Derler <david@derler.info>
2020-06-21 19:53:18 +0200

    Allow clippy::op_ref for some modules to prevent warnings for more efficient point/scalar multiplications

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,22 @@
 //! plan is to keep extending this to a library providing many modern cryptographic
 //! primitives as time permits.
 
+
+// We allow clippy::op_ref for some of the modules, because this warning fires 
+// when calling `&a * &b` when `a` and `b` are copy types. The warning suggests 
+// to do `a * b` instead but this would cause an implicit copy of the object 
+// in some cases, resulting in a performance penalty.  
+//
 /// Chameleon hashes.
+#[allow(clippy::op_ref)]
 pub mod chameleon_hashing;
 /// Encryption schemes.
+#[allow(clippy::op_ref)]
 pub mod encryption;
 /// Convenience functions for hashing.
 pub mod hashing;
 /// Zero-knowledge proofs, Sigma protocols, Fiat-Shamir transformation, Compositions, ...
+#[allow(clippy::op_ref)]
 pub mod zkproofs;
 
 use rand::thread_rng;
