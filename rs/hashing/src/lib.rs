b971659344facb50cd916f6d8cbfee65f69118f9
David Derler <david@derler.info>
2026-05-01 10:38:03 +0200

    Extend Hash trait

--- rs/hashing/src/lib.rs
+++ rs/hashing/src/lib.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests;
 
-use digest::{Digest, Reset};
+use digest::{Digest, FixedOutputReset, Output, Reset};
 
 pub struct DomainSeparator {
     bytes: Vec<u8>,
@@ -19,8 +19,26 @@ pub trait Hashable<H: Hash> {
     fn hash(&self, state: &mut H);
 }
 
+/// Provides several convenience methods related to domain separated hashing
 pub trait Hash: Digest {
+    /// Similar to `Digest::new`, but updates the digest with `domain_separator`
     fn new_with_separator(domain_separator: DomainSeparator) -> Self;
+
+    /// Similar to `Digest::finalize_reset`, but updates the digest with `domain_separator`
+    fn finalize_reset_with_separator(&mut self, domain_separator: DomainSeparator) -> Output<Self>
+    where
+        Self: FixedOutputReset;
+
+    /// Similar to `Digest::finalize_into_reset`, but updates the digest with 
+    /// `domain_separator`
+    fn finalize_into_reset_with_separator(
+        &mut self,
+        out: &mut Output<Self>,
+        domain_separator: DomainSeparator,
+    ) where
+        Self: FixedOutputReset;
+
+    /// Similar to `Digest::reset`, but updates the digest with `domain_separator`
     fn reset_with_separator(&mut self, domain_separator: DomainSeparator)
     where
         Self: Reset;
@@ -31,6 +49,26 @@ impl<D: Digest> Hash for D {
         let mut hash = D::new();
         hash.update(domain_separator);
         hash
+    }
+
+    fn finalize_reset_with_separator(&mut self, domain_separator: DomainSeparator) -> Output<Self>
+    where
+        Self: FixedOutputReset,
+    {
+        let output = digest::Digest::finalize_reset(self);
+        Digest::update(self, domain_separator);
+        output
+    }
+
+    fn finalize_into_reset_with_separator(
+        &mut self,
+        out: &mut Output<Self>,
+        domain_separator: DomainSeparator,
+    ) where
+        Self: FixedOutputReset,
+    {
+        digest::Digest::finalize_into_reset(self, out);
+        Digest::update(self, domain_separator);
     }
 
     fn reset_with_separator(&mut self, domain_separator: DomainSeparator)
--- rs/hashing/src/tests.rs
+++ rs/hashing/src/tests.rs
@@ -1,26 +1,24 @@
-use digest::{Digest, Reset};
+use digest::{Digest, generic_array::GenericArray};
 use sha2::Sha256;
 
 use crate::{DomainSeparator, Hash};
 
-fn initialize_hash_tuple<H: Hash + Digest + Reset>(
-    separator_string: &str,
-) -> (impl Digest, impl Hash + Reset) {
-    let mut h1 = H::new();
+fn initialize_hash_tuple(separator_string: &str) -> (Sha256, Sha256) {
+    let mut h1 = Sha256::new();
     Digest::update(
         &mut h1,
         format!("{}{}", separator_string.len(), separator_string),
     );
 
     let context = DomainSeparator::from_string(separator_string.to_string());
-    let h2 = H::new_with_separator(context);
+    let h2 = Sha256::new_with_separator(context);
 
     (h1, h2)
 }
 
 #[test]
 fn test_hashes_yield_same_result() {
-    let (h1, h2) = initialize_hash_tuple::<Sha256>("test");
+    let (h1, h2) = initialize_hash_tuple("test");
 
     assert_eq!(
         h1.chain_update("test").finalize().as_slice(),
@@ -29,14 +27,52 @@ fn test_hashes_yield_same_result() {
 }
 
 #[test]
-fn test_reset_hash_with_separator() {
+fn test_reset_with_separator() {
     let separator_string = "reset_test";
-    let (h1, mut h2) = initialize_hash_tuple::<Sha256>(separator_string);
+    let (h1, mut h2) = initialize_hash_tuple(separator_string);
 
     h2.update("this is a test");
     h2.reset_with_separator(DomainSeparator::from_string(separator_string.to_string()));
 
-    assert_eq!(h1.chain_update("test").finalize().as_slice(),
+    assert_eq!(
+        h1.chain_update("test").finalize().as_slice(),
+        h2.chain_update("test").finalize().as_slice()
+    );
+}
+
+#[test]
+fn test_finalize_reset_with_separator() {
+    let separator_string = "reset_test";
+    let separator = DomainSeparator::from_string(separator_string.to_string());
+    let (h1, h2) = initialize_hash_tuple(separator_string);
+
+    let h1_bytes = h1.chain_update("test").finalize();
+
+    let mut h2 = h2.chain_update("test");
+    let h2_bytes = h2.finalize_reset_with_separator(separator);
+    assert_eq!(h1_bytes.as_slice(), h2_bytes.as_slice());
+
+    assert_eq!(
+        h1_bytes.as_slice(),
+        h2.chain_update("test").finalize().as_slice()
+    );
+}
+
+#[test]
+fn test_finalize_into_reset_with_separator() {
+    let separator_string = "into_reset_test";
+    let separator = DomainSeparator::from_string(separator_string.to_string());
+    let (h1, h2) = initialize_hash_tuple(separator_string);
+
+    let h1_bytes = h1.chain_update("test").finalize();
+
+    let mut h2 = h2.chain_update("test");
+    let mut h2_bytes = GenericArray::default();
+    h2.finalize_into_reset_with_separator(&mut h2_bytes, separator);
+    assert_eq!(h1_bytes.as_slice(), h2_bytes.as_slice());
+
+    assert_eq!(
+        h1_bytes.as_slice(),
         h2.chain_update("test").finalize().as_slice()
     );
 }
